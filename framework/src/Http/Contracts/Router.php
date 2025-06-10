10368a571b75c35ddddbe4d2ea59cea8e93ba5f3
Ollie Read <code@ollieread.com>
2025-06-10 12:21:47 +0100

    chore: Refactor and rejig

--- framework/src/Http/Contracts/Router.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Popcorn\Http\Contracts;
-
-interface Router
-{
-
-}
--- framework/src/Http/Routing/RouteDiscovery.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Popcorn\Http\Routing;
-
-class RouteDiscovery
-{
-
-}
